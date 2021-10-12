4920a07ea0a9aca39e0e52d9c6d92971c547ff94
KanemotoHiroka177 <s1f101901533@iniad.org>
2021-10-13 03:28:14 +0900

    css_ver3マウスアクション

--- static/js/style.js
+++ static/js/style.js
@@ -5,6 +5,7 @@ script.setAttribute('src', 'https://code.jquery.com/jquery-1.12.4.min.js')
 
 script.setAttribute('type', 'text/javascript')
 script.addEventListener('load', function() {
+    //↓スクロールアニメーション
 $(function() {
     // スクロールしたときに実行
     $(window).scroll(function () {
@@ -23,6 +24,35 @@ $(function() {
        }
     });
  });
+
+    //↓マウスアクション
+ if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+    $(function(){
+        var body = document.body;
+        var cursor = document.createElement("div");
+        var stalker = document.createElement("div");
+        cursor.id = "cursor";
+        stalker.id = "stalker";
+        body.appendChild(cursor);
+        body.appendChild(stalker);
+        body.addEventListener("mousemove", function(e) {
+              cursor.style.left = e.clientX + "px";
+              cursor.style.top = e.clientY + "px";
+              stalker.style.left = e.clientX + "px";
+              stalker.style.top = e.clientY + "px";
+        }, false);
+
+        $("a").on("mouseenter", function() {
+            $('#cursor').addClass("active");
+            $('#stalker').addClass("active");
+        });
+
+        $("a").on("mouseleave", function() {
+            $('#cursor').removeClass("active");
+            $('#stalker').removeClass("active");
+        });
+    });
+}
 
 })
 document.head.appendChild(script)
