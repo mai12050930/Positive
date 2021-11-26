df1686bf36387ef02f7bfd69ea9eeea868192bfb
mai12050930 <mai00saw@gmail.com>
2021-11-26 22:01:22 +0900

    username,chat

--- config/asgi.py
+++ config/asgi.py
@@ -1,3 +1,12 @@
+"""
+ASGI config for mysite project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
 import os
 
 from django.core.asgi import get_asgi_application
@@ -7,10 +16,10 @@ import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-#application = get_asgi_appication()
+#application = get_asgi_application()
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http':django_asgi_app,
+    'http':get_asgi_application(),
     'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
 })
--- static/js/chat.js
+++ static/js/chat.js
@@ -1,44 +1,90 @@
-const g_elementInputMessage = document.getElementById("input_nessage");
+const g_elementDivJoinScreen = document.getElementById("div_join_screen");
+const g_elementDivChatScreen = document.getElementById("div_chat_screen");
+const g_elementInputUserName = document.getElementById("input_username");
+
+const g_elementTextUserName = document.getElementById("text_username");
+
+const g_elementInputMessage = document.getElementById("input_message");
 const g_elementListMessage = document.getElementById("list_message");
+
 
 //WebSocketオブジェクト
 let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
 const g_socket = new WebSocket(ws_scheme + "://" + window.location.host + "/ws/chat/");
 
-//Sendボタンを押したときの処理
-function onsubmitButton_Send(){
-    //送信用テキストHTML要素からメッセージ文字列取得
-    let strMessage = g_elementInputMessage.value;
-    if(!strMessage){
+//Joinボタンを押した時の処理
+function onsubmitButton_JoinChat(){
+    //ユーザー名
+    let strInputUserName = g_elementInputUserName.value;
+    if(!strInputUserName){
         return;
     }
+    g_elementTextUserName.value = strInputUserName;
 
+    //サーバーにjoin送信
+    g_socket.send(JSON.stringify({"data_type":"join", "username": strInputUserName }));
+
+    //画面の切り替え
+    g_elementDivJoinScreen.style.display = "none"; //参加画面の非表示
+    g_elementDivChatScreen.style.display = "block"; //チャット画面の表示
+}
+
+//Leave Chatボタンを押すと呼ばれる関数
+function onclickButton_LeaveChat(){
+    //メッセージリストのクリア
+    while(g_elementListMessage.firstChild){
+        g_elementListMessage.removeChild(g_elementListMessage.firstChild);
+    }
+
+    //ユーザー名
+    g_elementTextUserName.value = "";
+
+    //サーバーに"leave"を送信
+    g_socket.send(JSON.stringify({"data_type":"leave"}));
+
+    //画面の切り替え
+    g_elementDivChatScreen.style.display = "none"; //チャット画面の非表示
+    g_elementDivJoinScreen.style.display = "flex"; //参加画面の表示
+}
+
+//Sendボタンを押したときの処理
+function onsubmitButton_Send() {
+    //送信用テキストHTML要素からメッセージ文字列取得
+    let strMessage = g_elementInputMessage.value;
+    if (!strMessage) {
+        return;
+    }
     //WebSocketを通したメッセージ送信
-    g_socket.send(JSON.stringify({"message":strMessage}));
+    g_socket.send(JSON.stringify({
+        "message": strMessage
+    }));
 
     //送信用テキストHTML要素の中身クリア
     g_elementInputMessage.value = "";
 }
 
 //WebSocketからメッセージ受信時の処理
-g_socket.onmessage = (event) =>
-{
+g_socket.onmessage = (event) => {
+    //自身がまだ参加していない時は無視
+    if(!g_elementTextUserName.value){
+        return;
+    }
+    
     //テキストデータをJSONデータにデコード
     let data = JSON.parse(event.data);
 
     //メッセージの整形
-    let strMessage = data["message"];
+    let strMessage = data["datetime"] + "-[" + data["username"] + "]" + data["message"];
 
     //拡散されたメッセージをメッセージリストに追加
     let elementLi = document.createElement("li");
     elementLi.textContent = strMessage;
     g_elementListMessage.prepend(elementLi); //リストの一番上に追加
     //g_elementListMessage.append(elementLi); //リストの一番下に追加
-}
-    
+};
+
 //WebSocketクローズ時の処理
-g_socket.onclose = (event) =>
-{
+g_socket.onclose = (event) => {
     //ウェブページを閉じたとき以外のWebSocketクローズは想定外
     console.error("Unexpected : Chat socket closed.");
 };
