bb0dbe4785a60ad6b57fb10523f6756b3c21ea7a
mai12050930 <mai00saw@gmail.com>
2022-01-11 23:02:25 +0900

    掲示板画面変更

--- config/local_setting.py
+++ config/local_setting.py
@@ -1,9 +1,6 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '93h$aa_(@=0vlw^itbuhgv^vb!6k$p@!#n0)-9fd5g#s1_e!ta'
-
-
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
